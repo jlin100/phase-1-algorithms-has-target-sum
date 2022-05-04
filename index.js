3775323dceccbc7c41b42a16f4894b0c603b23ea
Jeff Lin <thejefflin@gmail.com>
2022-05-03 23:31:06 -0400

    commit tree 270e5396bebfef87e306eedd5c3e4ee300d8a6a0

--- index.js
+++ index.js
@@ -1,9 +1,20 @@
 function hasTargetSum(array, target) {
   // Write your algorithm here
+  const seenNumbers = {};
+
+  for (const number of array) {
+    // n steps 
+    const complement = target - number;
+    if (complement in seenNumbers) return true
+    seenNumbers[number] = true
+  }
+  return false 
 }
 
 /* 
   Write the Big O time complexity of your function here
+  Runtime: 0(n^2)
+  Space: 0(n)
 */
 
 /* 
